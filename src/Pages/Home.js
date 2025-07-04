d5681406a6383a494a197ba344723c50d06ad25d
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-07-04 21:43:22 +0530

    Create Home.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My E-commerce Site</h1>
+      <p>Browse our amazing products!</p>
+    </div>
+  );
+}
+
+export default Home;
