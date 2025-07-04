6b8a24bf3f4d89194c35cc529cfd89195ec0300b
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-07-04 21:38:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Product from './pages/Product';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
